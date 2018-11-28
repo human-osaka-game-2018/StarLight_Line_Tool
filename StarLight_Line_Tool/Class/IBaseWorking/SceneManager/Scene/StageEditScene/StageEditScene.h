48539b05a5683b29f27e87e8e73993442dacdf8c
Harutaka-Tsujino <glaca986@outlook.jp>
2018-11-28 12:34:46 +0900

    新しく追加したシーンの追加

---
+++ StarLight_Line_Tool/Class/IBaseWorking/SceneManager/Scene/StageEditScene/StageEditScene.h
@@ -0,0 +1,22 @@
+﻿#ifndef STAGE_EDIT_SCENE_H
+#define STAGE_EDIT_SCENE_H
+
+#include <windows.h>
+
+#include "../Scene.h"
+#include "../ObjManager/Obj/Obj.h"
+#include "../ObjManager/Obj/StageEditSceneObj/StageEditSceneObj.h"
+
+class StageEditScene :public Scene
+{
+public:
+	StageEditScene()
+	{
+		m_pObjManager->Resister(static_cast<Obj*>(new StageEditBack()));
+		m_pObjManager->Resister(static_cast<Obj*>(new StageEditStars()));
+	}
+
+	~StageEditScene() {};
+};
+
+#endif //! STAGE_EDIT_SCENE_H
