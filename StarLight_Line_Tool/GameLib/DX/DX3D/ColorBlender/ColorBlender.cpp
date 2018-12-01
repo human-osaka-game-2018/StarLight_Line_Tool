af7639bd919957ad88875a271d2e21744ac14dd3
Harutaka-Tsujino <glaca986@outlook.jp>
2018-12-01 18:55:35 +0900

    ドット単位で描画しないアルファ値の設定

--- StarLight_Line_Tool/GameLib/DX/DX3D/ColorBlender/ColorBlender.cpp
+++ StarLight_Line_Tool/GameLib/DX/DX3D/ColorBlender/ColorBlender.cpp
@@ -11,7 +11,7 @@ VOID ColorBlender::DefaultColorBlending() const
 	m_rpDX3D_DEV->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
 	m_rpDX3D_DEV->SetRenderState(D3DRS_ALPHATESTENABLE, TRUE);
 	m_rpDX3D_DEV->SetRenderState(D3DRS_ALPHAFUNC, D3DCMP_GREATEREQUAL);
-	m_rpDX3D_DEV->SetRenderState(D3DRS_ALPHAREF, 0x00);
+	m_rpDX3D_DEV->SetRenderState(D3DRS_ALPHAREF, 0x22);
 
 	m_rpDX3D_DEV->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE);
 	m_rpDX3D_DEV->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_MODULATE);
